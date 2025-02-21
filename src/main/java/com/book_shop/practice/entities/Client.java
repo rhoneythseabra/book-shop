f89ef96885aebf9e60986f483c2ce4c9939fead6
rhoney seabra <rhoneyholanda@gmail.com>
2025-02-21 15:45:21 -0300

    create a file import.sql and feed him. And configure H2 database.

--- src/main/java/com/book_shop/practice/entities/Client.java
+++ src/main/java/com/book_shop/practice/entities/Client.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,6 +18,10 @@ public class Client {
 	
 	private String name;
 	private String email;
+	
+	@ManyToOne
+	@JoinColumn(name = "book_id")
+	private Book book;
 	
 	public Client() {
 	}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_book(name, genre) VALUES ('Lord of The Rings', 'Fantasy');
+INSERT INTO tb_book(name, genre) VALUES ('50 Shades of Gray', 'Fetich');
+INSERT INTO tb_book(name, genre) VALUES ('New Moon - A Twilight Saga', 'Drama/Teen');
+INSERT INTO tb_book(name, genre) VALUES ('The Batman', 'Comic/HQ');
+
+INSERT INTO tb_client(name, email) VALUES ('Bianca', 'bianca@gmail.com');
+INSERT INTO tb_client(name, email) VALUES ('Rhoney', 'rhoney@gmail.com');
